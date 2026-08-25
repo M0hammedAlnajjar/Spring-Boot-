0da39ccb83ebbbf8f0372361feeaa4aa55590a63
mohammed <mooodo150@gmail.com>
2026-08-25 16:30:16 +0400

    feat: add task number generator

--- demo/demo/src/main/java/com/cl/demo/services/TaskService.java
+++ demo/demo/src/main/java/com/cl/demo/services/TaskService.java
@@ -1,9 +1,14 @@
 package com.cl.demo.services;
 import org.springframework.stereotype.Service;
-@Service
 
+import java.util.UUID;
+
+@Service
 public class TaskService {
 
     public static final String Task_SAVED = "Task_SAVED";
+public String  generateTaskNumber(){
+    return "TASK-"+ UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+}
 
 }
