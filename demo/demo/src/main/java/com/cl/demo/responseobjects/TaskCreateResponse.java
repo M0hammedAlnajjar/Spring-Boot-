94b970fda26142fe2f87d375ecb92bba0be2b0c9
mohammed <mooodo150@gmail.com>
2026-08-25 15:12:54 +0400

    feat: add task list response conversion

--- demo/demo/src/main/java/com/cl/demo/responseobjects/TaskCreateResponse.java
+++ demo/demo/src/main/java/com/cl/demo/responseobjects/TaskCreateResponse.java
@@ -4,6 +4,7 @@ import com.cl.demo.entities.Task;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -18,6 +19,7 @@ public class TaskCreateResponse {
     private String dueDate;
     private Boolean isAssigned;
 
+    // Convert one Task
     public static TaskCreateResponse convert(Task task) {
 
         if (task == null || task.getId() == null) {
@@ -33,12 +35,39 @@ public class TaskCreateResponse {
         response.setTaskNumber(task.getTaskNumber());
 
         if (task.getTaskStatus() != null) {
-            response.setTaskStatus(task.getTaskStatus().toString());
+            response.setTaskStatus(
+                    task.getTaskStatus().toString()
+            );
         }
+
         if (task.getDueDate() != null) {
-            response.setDueDate(task.getDueDate().toString());
+            response.setDueDate(
+                    task.getDueDate().toString()
+            );
         }
+
         response.setIsAssigned(task.getIsAssigned());
+
         return response;
+    }
+
+    // Convert a List of Tasks
+    public static List<TaskCreateResponse> convert(
+            List<Task> tasks
+    ) {
+
+        List<TaskCreateResponse> responses =
+                new ArrayList<>();
+
+        for (Task task : tasks) {
+
+            TaskCreateResponse response = convert(task);
+
+            if (response != null) {
+                responses.add(response);
+            }
+        }
+
+        return responses;
     }
 }
