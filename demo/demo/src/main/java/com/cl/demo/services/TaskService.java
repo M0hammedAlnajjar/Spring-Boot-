51344a8d4b90b2eb863afa74330d06267f9c47fb
mohammed <mooodo150@gmail.com>
2026-08-25 16:25:40 +0400

    feat: create task service

---
+++ demo/demo/src/main/java/com/cl/demo/services/TaskService.java
@@ -0,0 +1,9 @@
+package com.cl.demo.services;
+import org.springframework.stereotype.Service;
+@Service
+
+public class TaskService {
+
+    public static final String Task_SAVED = "Task_SAVED";
+
+}
