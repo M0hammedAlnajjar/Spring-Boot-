0d60188475903557bb0633a7c1025d75c0f82e2a
mohammed <mooodo150@gmail.com>
2026-08-25 16:08:42 +0400

    feat: create task update response fields

---
+++ demo/demo/src/main/java/com/cl/demo/responseobjects/TaskUpdateResponse.java
@@ -0,0 +1,12 @@
+package com.cl.demo.responseobjects;
+import com.cl.demo.entities.Task;
+
+public class TaskUpdateResponse {
+    private String taskId;
+    private String title;
+    private String description;
+    private String taskNumber;
+    private String taskStatus;
+    private String dueDate;
+    private Boolean isAssigned;
+}
