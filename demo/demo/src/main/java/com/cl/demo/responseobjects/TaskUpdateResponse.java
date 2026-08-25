66ff3314e96d0d78c3e8e3aebeace66a23a08dcd
mohammed <mooodo150@gmail.com>
2026-08-25 16:14:55 +0400

    feat: add single task update conversion

--- demo/demo/src/main/java/com/cl/demo/responseobjects/TaskUpdateResponse.java
+++ demo/demo/src/main/java/com/cl/demo/responseobjects/TaskUpdateResponse.java
@@ -1,6 +1,11 @@
 package com.cl.demo.responseobjects;
 import com.cl.demo.entities.Task;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
 public class TaskUpdateResponse {
     private String taskId;
     private String title;
@@ -9,4 +14,27 @@ public class TaskUpdateResponse {
     private String taskStatus;
     private String dueDate;
     private Boolean isAssigned;
+
+    public static TaskUpdateResponse convert(Task task) {
+        if (task == null || task.getId() == null){
+        return null;
+    }
+        TaskUpdateResponse taskUpdateResponse = new TaskUpdateResponse();
+        taskUpdateResponse.setTaskId(task.getId().toString());
+        taskUpdateResponse.setTitle(task.getTitle());
+        taskUpdateResponse.setDescription(task.getDescription());
+        taskUpdateResponse.setTaskNumber(task.getTaskNumber());
+
+        if (task.getTaskStatus() != null){
+            taskUpdateResponse.setTaskStatus(task.getTaskStatus().toString());
+
+        }
+        if (task.getDueDate() != null){
+            taskUpdateResponse.setDueDate(task.getDueDate().toString());
+        }
+        return taskUpdateResponse;
+
+}
+
+
 }
