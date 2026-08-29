8f005edc3392199db0796f0a944d49035113f382
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-29 22:46:17 +0400

    fix: align task controller with task service

--- demo/demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,25 +1,26 @@
 package com.cl.demo.controllers;
-import com.cl.demo.services.TaskService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.cl.demo.requestobjects.TaskCreateRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import com.cl.demo.responseobjects.TaskCreateResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.util.List;
-import com.cl.demo.requestobjects.TaskUpdateRequest;
-import com.cl.demo.responseobjects.TaskUpdateResponse;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
+import com.cl.demo.requestobjects.TaskCreateRequest;
+import com.cl.demo.requestobjects.TaskUpdateRequest;
+import com.cl.demo.responseobjects.TaskCreateResponse;
+import com.cl.demo.responseobjects.TaskUpdateResponse;
+import com.cl.demo.services.TaskService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 import java.util.Map;
+
 @RestController
 @RequestMapping("task")
-
-
 public class TaskController {
+
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) {
@@ -33,30 +34,40 @@ public class TaskController {
 
         return taskService.addTask(taskCreateRequest);
     }
+
     @GetMapping("/getById")
     public TaskCreateResponse getTaskById(
             @RequestParam String uuid
     ) {
 
-        return taskService.getTaskById(uuid);
+        return TaskCreateResponse.convert(
+                taskService.getTaskById(uuid)
+        );
     }
+
     @GetMapping("/getAll")
     public List<TaskCreateResponse> getAllTasks() {
 
-        return taskService.getAllTasks();
+        return TaskCreateResponse.convert(
+                taskService.getAllTasks()
+        );
     }
+
     @PutMapping("/update")
     public TaskUpdateResponse updateTask(
             @RequestBody TaskUpdateRequest taskUpdateRequest
     ) {
 
-        return taskService.updateTask(taskUpdateRequest);
+        return TaskUpdateResponse.convert(
+                taskService.updateTask(taskUpdateRequest)
+        );
     }
+
     @DeleteMapping("/deleteById")
-    public Map<String, String> deleteTaskById(
-            @RequestParam String uuid
+    public Boolean deleteTaskById(
+            @RequestParam String id
     ) {
 
-        return taskService.deleteTaskById(uuid);
+        return taskService.deleteById(id);
     }
 }
