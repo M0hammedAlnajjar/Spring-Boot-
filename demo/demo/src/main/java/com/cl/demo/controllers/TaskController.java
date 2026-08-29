5cac932ba889c03a737044eb1acef725860436f7
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-29 19:17:43 +0400

    feat: add task creation endpoint

--- demo/demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -2,6 +2,11 @@ package com.cl.demo.controllers;
 import com.cl.demo.services.TaskService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.cl.demo.requestobjects.TaskCreateRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 @RestController
 @RequestMapping("task")
 
@@ -11,5 +16,13 @@ public class TaskController {
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @PostMapping("/add")
+    public Map<String, String> addTask(
+            @RequestBody TaskCreateRequest taskCreateRequest
+    ) {
+
+        return taskService.addTask(taskCreateRequest);
     }
 }
