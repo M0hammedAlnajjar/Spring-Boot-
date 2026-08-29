744fd6e70d8171aebf749bd75db8150d607fa359
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-29 19:16:24 +0400

    feat: create task controller

--- demo/demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,4 +1,15 @@
 package com.cl.demo.controllers;
+import com.cl.demo.services.TaskService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("task")
+
 
 public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 }
