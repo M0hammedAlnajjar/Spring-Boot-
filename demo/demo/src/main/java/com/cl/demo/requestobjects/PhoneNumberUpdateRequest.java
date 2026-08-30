28a509eaeeef7273c9bdb9d90d92a2460467a287
mohammed <mooodo150@gmail.com>
2026-08-30 10:46:30 +0400

    feat: add phone number update request

---
+++ demo/demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.cl.demo.requestobjects;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class PhoneNumberUpdateRequest {
+    private String uuid;
+    private String countryCodeToUpdate;
+    private Long phoneNumberToUpdate;
+}
