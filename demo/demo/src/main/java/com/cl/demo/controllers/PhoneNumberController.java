f6073e40c40371d1a3c4fb45ab2f34db0cb0a10f
mohammed <mooodo150@gmail.com>
2026-08-31 07:46:55 +0400

    feat: add phone number create endpoint

--- demo/demo/src/main/java/com/cl/demo/controllers/PhoneNumberController.java
+++ demo/demo/src/main/java/com/cl/demo/controllers/PhoneNumberController.java
@@ -3,6 +3,12 @@ import com.cl.demo.services.PhoneNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cl.demo.entities.PhoneNumber;
+import com.cl.demo.requestobjects.PhoneNumberCreateRequest;
+import com.cl.demo.responseobjects.PhoneNumberCreateResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("phoneNumber")
 
@@ -11,4 +17,17 @@ public class PhoneNumberController {
         @Autowired
         private PhoneNumberService phoneNumberService;
 
+
+    @PostMapping("/add")
+    public PhoneNumberCreateResponse addPhoneNumber(
+            @RequestBody PhoneNumberCreateRequest request
+    ) {
+
+        PhoneNumber phoneNumber =
+                phoneNumberService.addPhoneNumber(request);
+
+        return PhoneNumberCreateResponse.convert(
+                phoneNumber
+        );
+    }
 }
